f2f908774430eb050192790c7f3d1b61519755b4
priyanshu singh <priy2032004anshu.com>
2026-01-15 00:12:52 +0530

    Fix file path using pathlib for CI-safe execution

--- scripts/extract.py
+++ scripts/extract.py
@@ -4,8 +4,11 @@
 
 
 
-
 import pandas as pd
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+DATA_PATH = BASE_DIR / "data" / "sales.csv"
 
 def extract():
-    return pd.read_csv("data/sales.csv")
+    return pd.read_csv(DATA_PATH)
